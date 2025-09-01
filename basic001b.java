30e1938044031788f16d2e4ec5fd426bd3848775
Vũ Đức Anh <stevethegglord@gmail.com>
2025-09-01 16:55:24 +0700

    Delete basic001b.java

--- basic001b.java
+++
@@ -1,9 +0,0 @@
-
-public class basic001b {
-    public static void main(String[] args) {
-        String hello = "hello";
-        String name = "steve";
-
-        System.out.printf("%s, %s", hello.toUpperCase(), name.toLowerCase());
-    }
-}
